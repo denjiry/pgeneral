d196355a8bcffacbc34b3040f5f3adf365673ece
kento tajiri <k.denjiry@gmail.com>
2018-09-02 11:51:59 +0900

    Add sample but not work (Error: No such hypothesis: H)

--- summmit.v
+++ summmit.v
@@ -2,7 +2,26 @@ Inductive bool : Set :=
 |true : bool
 |false : bool.
 
+Inductive False : Prop:=.
 
+Inductive nat : Set :=
+|O : nat
+|S : nat -> nat.
 
-1<>2
+Inductive True : Prop :=
+I : True.
+
+(* Inductive eq (A: Type) (x : A) : A-> Prop:= *)
+(* eq_refl : x = x :>A. *)
+  
+Definition not (A : Prop) := A-> False.
+
+Notation "~ x" := (not x).
+
+Theorem sample : 1 <> 2.
+Proof.
+  unfold not.
+  intros.
+  (* Error: No such hypothesis: H *)
+  inversion H. Qed.
 
