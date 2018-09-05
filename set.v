7351bde877fbc68f75840f8d964751bb7de8eb91
kento tajiri <k.denjiry@gmail.com>
2018-09-05 23:12:13 +0900

    Add set.v for mathcomp/ssreflect

--- set.v
+++ set.v
@@ -1,7 +1,15 @@
 From mathcomp
      Require Import all_ssreflect.
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
 
 Definition mySet (M : Type) := M -> Prop.
+Definition belong {M : Type} (A : mySet M) (x : M) :
+  Prop := A x.
+Notation "x \In A" := (belong A x) (at level 11).
+Axiom axiom_myset : forall (M : Type) (A : mySet M),
+    forall (x : M), (x \In A) \/ ~(x \In A).
+
+Definition myEmptySet {M : Type} : mySet M := fun _ => False.
