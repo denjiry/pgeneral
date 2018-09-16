e7fa78a5b5523e7e6451005d43d60d1bdb5187e5
kento tajiri <k.denjiry@gmail.com>
2018-09-17 03:44:35 +0900

    Advance text

--- basicsj.v
+++ basicsj.v
@@ -85,3 +85,54 @@ Proof. simpl. reflexivity. Qed.
 Check (negb true).
 
 Check negb.
+
+
+Module Playground1.
+Inductive nat : Type :=
+  | O : nat
+  | S : nat -> nat.
+
+Definition pred (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+
+End Playground1.
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+    | O => O
+    | S O => O
+    | S (S n') => n'
+  end.
+
+Check (S (S (S (S O)))).
+Eval simpl in (minustwo 4).
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+
+Example test_oddb1: (oddb (S O)) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_oddb2: (oddb (S (S (S (S O))))) = false.
+Proof. simpl. reflexivity. Qed.
+
+Module Playground2.
+
+Fixpoint plus (n : nat) (m : nat) : nat :=
+  match n with
+    | O => m
+    | S n' => S (plus n' m)
+  end.
+
+Eval simpl in (plus (S (S (S O))) (S (S O))).
