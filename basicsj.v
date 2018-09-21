ff29dc155b715831706cdb2de6e4a054b7d81b7e
kento tajiri <k.denjiry@gmail.com>
2018-09-22 01:40:00 +0900

    finish before plus_id_exercise

--- basicsj.v
+++ basicsj.v
@@ -220,3 +220,38 @@ Proof. compute. reflexivity. Qed.
 Example test_blt_nat3: (blt_nat 4 2) = false.
 Proof. compute. reflexivity. Qed.
 
+Theorem plus_O_n : forall n:nat, 0 + n = n.
+Proof.
+  simpl. reflexivity.  Qed.
+
+Theorem plus_O_n' : forall n:nat, 0 + n = n.
+Proof.
+  reflexivity.  Qed.
+
+Eval simpl in (forall n:nat, n + 0 = n).
+
+Eval simpl in (forall n:nat, 0 + n = n).
+
+Theorem plus_O_n'' : forall n:nat, 0 + n = n.
+Proof.
+  intros n. reflexivity. Qed.
+
+Theorem plus_1_l : forall n:nat, 1 + n = S n.
+Proof.
+  intros n. reflexivity.  Qed.
+
+Theorem mult_0_l : forall n:nat, 0 * n = 0.
+Proof.
+  intros n. reflexivity.  Qed.
+
+Theorem plus_id_example : forall n m:nat,
+  n = m ->
+  n + n = m + m.
+Proof.
+  intros n m. intros H. rewrite <- H. reflexivity.
+Qed.
+
+Theorem plus_id_exercise : forall n m o : nat,
+  n = m -> m = o -> n + m = m + o.
+Proof.
+  
