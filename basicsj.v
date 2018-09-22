e02f4d0e47cff6ae20dc297eb20896eab49c7a78
kento tajiri <k.denjiry@gmail.com>
2018-09-23 01:43:04 +0900

    Solve mult_1_plus

--- basicsj.v
+++ basicsj.v
@@ -259,3 +259,17 @@ Proof.
   reflexivity.
 Qed.
 
+Theorem mult_0_plus : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  rewrite -> plus_O_n.
+  reflexivity.
+Qed.
+
+Theorem mult_1_plus : forall n m : nat,
+  (1 + n) * m = m + (n * m).
+Proof.
+  intros n m. reflexivity.
+Qed.
+
