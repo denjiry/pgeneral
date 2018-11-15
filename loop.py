0f9eda1ddaeda2676c03ad4b27fc49a63e5d4162
kento tajiri <k.denjiry@gmail.com>
2018-11-15 19:52:46 +0900

    Add And & Or

--- loop.py
+++ loop.py
@@ -17,16 +17,60 @@ class Not(Term):
         return 'Not('+self.term.__repr__()+')'
 
 
+class And(Term):
+    def __init__(self, term1, term2):
+        assert isinstance(term1, Term) and isinstance(term2, Term)
+        self.term1 = term1
+        self.term2 = term2
+        return
+
+    def __repr__(self):
+        return '('+self.term1.__repr__()+' & '+self.term2.__repr__()+')'
+
+
+class Or(Term):
+    def __init__(self, term1, term2):
+        assert isinstance(term1, Term) and isinstance(term2, Term)
+        self.term1 = term1
+        self.term2 = term2
+        return
+
+    def __repr__(self):
+        return '('+self.term1.__repr__()+' or '+self.term2.__repr__()+')'
+
+
 def tableu(prop_list):
     return True
 
 
-def main():
+def test_and_or():
+    a = Term('a')
+    b = Term('b')
+    aandb = And(a, b)
+    aornotb = Or(a, Not(b))
+    aornotborb = Or(aornotb, b)
+    print(aandb)
+    print(aornotb)
+    print(aornotborb)
+    return
+
+
+def test_Not():
     a = Term('a')
     nota = Not(a)
     notnota = Not(nota)
     print(nota)
     print(notnota)
+    return
+
+
+def test():
+    test_Not()
+    test_and_or()
+    return
+
+
+def main():
     ex = [['a', 'and', 'b'],
           ['b', 'or', 'c'],
           ['a', '>', 'd']]
@@ -38,4 +82,5 @@ def main():
 
 
 if __name__ == '__main__':
+    test()
     main()
