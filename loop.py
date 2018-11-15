79530b6735a74e1ed338bc80376abe44bb7518d5
kento tajiri <k.denjiry@gmail.com>
2018-11-15 19:32:44 +0900

    Add Term & Not

--- loop.py
+++ loop.py
@@ -1,8 +1,32 @@
+class Term():
+    def __init__(self, name=None):
+        self.name = name
+        return
+
+    def __repr__(self):
+        return self.name if self.name else 'term'
+
+
+class Not(Term):
+    def __init__(self, term):
+        assert isinstance(term, Term)
+        self.term = term
+        return
+
+    def __repr__(self):
+        return 'Not('+self.term.__repr__()+')'
+
+
 def tableu(prop_list):
     return True
 
 
 def main():
+    a = Term('a')
+    nota = Not(a)
+    notnota = Not(nota)
+    print(nota)
+    print(notnota)
     ex = [['a', 'and', 'b'],
           ['b', 'or', 'c'],
           ['a', '>', 'd']]
