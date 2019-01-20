bec6aee97499ccad6a2966794f16a605e64fa2ab
kento tajiri <k.denjiry@gmail.com>
2019-01-21 03:17:33 +0900

    Proof plus_swap' with tactic 'replace'

--- basicsj.v
+++ basicsj.v
@@ -497,3 +497,13 @@ Proof.
   Case "m = S m'".
   simpl. rewrite <- IHm'. rewrite <- mult_help. reflexivity. Qed.
 
+Theorem plus_swap' : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  intros n m p.
+  rewrite -> plus_assoc.
+  rewrite -> plus_assoc.
+  replace (n + m) with (m + n).
+  reflexivity.
+  rewrite -> plus_comm. reflexivity. Qed.
+
