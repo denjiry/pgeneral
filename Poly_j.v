3917abf0da01f633c09ea3122cff1c5575295f36
kento tajiri <k.denjiry@gmail.com>
2019-03-01 05:34:50 +0900

    Complete Poly_J

--- Poly_j.v
+++ Poly_j.v
@@ -1,6 +1,256 @@
-Require Export basicsj.
+Require Export Lists_J.
+
+Inductive boollist : Type :=
+  | bool_nil : boollist
+  | bool_cons : bool -> boollist -> boollist.
+
+Inductive list (X:Type) : Type :=
+  | nil
+  | cons (x : X) (l : list X).
+
+Check (cons nat 2 (cons nat 1 (nil nat))).
+
+Fixpoint repeat (X : Type) (x : X) (count : nat) : list X :=
+  match count with
+  | 0 => nil X
+  | S count' => cons X x (repeat X x count')
+  end.
+
+Example test_repeat1 :
+  repeat nat 4 2 = cons nat 4 (cons nat 4 (nil nat)).
+Proof. reflexivity. Qed.
+
+Module MumbleGrumble.
+Inductive mumble : Type :=
+  | a
+  | b (x : mumble) (y : nat)
+  | c.
+Inductive grumble (X:Type) : Type :=
+  | d (m : mumble)
+  | e (x : X).
+End MumbleGrumble.
+(* Definition manual_grade_for_mumble_grumble : option (nat*string) := None. *)
+
+Fixpoint repeat' X x count : list X :=
+  match count with
+  | 0 => nil X
+  | S count' => cons X x (repeat' X x count')
+  end.
+
+Arguments nil {X}.
+Arguments cons {X} _ _.
+Arguments repeat {X} x count.
+
+Definition list123'' := cons 1 (cons 2 (cons 3 nil)).
+
+Fixpoint repeat''' {X : Type} (x : X) (count : nat) : list X :=
+  match count with
+  | 0 => nil
+  | S count' => cons x (repeat''' x count')
+  end.
+
+Inductive list' {X:Type} : Type :=
+  | nil'
+  | cons' (x : X) (l : list').
+
+Fixpoint app {X : Type} (l1 l2 : list X)
+             : (list X) :=
+  match l1 with
+  | nil => l2
+  | cons h t => cons h (app t l2)
+  end.
+Fixpoint rev {X:Type} (l:list X) : list X :=
+  match l with
+  | nil => nil
+  | cons h t => app (rev t) (cons h nil)
+  end.
+Fixpoint length {X : Type} (l : list X) : nat :=
+  match l with
+  | nil => 0
+  | cons _ l' => S (length l')
+  end.
+Example test_rev1 :
+  rev (cons 1 (cons 2 nil)) = (cons 2 (cons 1 nil)).
+Proof. reflexivity. Qed.
+Example test_rev2:
+  rev (cons true nil) = cons true nil.
+Proof. reflexivity. Qed.
+Example test_length1: length (cons 1 (cons 2 (cons 3 nil))) = 3.
+Proof. reflexivity. Qed.
+
+Fail Definition mynil := nil.
+
+Definition mynil : list nat := nil.
+
+Definition mynil' := @nil nat.
+
+Notation "x :: y" := (cons x y)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y []) ..).
+Notation "x ++ y" := (app x y)
+                     (at level 60, right associativity).
+
+Definition list123''' := [1; 2; 3].
+
+Theorem app_nil_r : forall(X:Type), forall l:list X,
+  l ++ [] = l.
+Proof.
+  (* FILL IN HERE *) Admitted.
+Theorem app_assoc : forall A (l m n:list A),
+  l ++ m ++ n = (l ++ m) ++ n.
+Proof.
+  (* FILL IN HERE *) Admitted.
+Lemma app_length : forall(X:Type) (l1 l2 : list X),
+  length (l1 ++ l2) = length l1 + length l2.
+Proof.
+(* FILL IN HERE *) Admitted.
+
+Theorem rev_app_distr: forall X (l1 l2 : list X),
+  rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  (* FILL IN HERE *) Admitted.
+Theorem rev_involutive : forall X : Type, forall l : list X,
+  rev (rev l) = l.
+Proof.
+(* FILL IN HERE *) Admitted.
+
+Inductive prod (X Y : Type) : Type :=
+| pair (x : X) (y : Y).
+Arguments pair {X} {Y} _ _.
+
+Notation "( x , y )" := (pair x y).
+
+Notation "X * Y" := (prod X Y) : type_scope.
+
+Definition fst {X Y : Type} (p : X * Y) : X :=
+  match p with
+  | (x, y) => x
+  end.
+
+Definition snd {X Y : Type} (p : X * Y) : Y :=
+  match p with
+  | (x, y) => y
+  end.
+
+Fixpoint combine {X Y : Type} (lx : list X) (ly : list Y)
+           : list (X*Y) :=
+  match lx, ly with
+  | [], _ => []
+  | _, [] => []
+  | x :: tx, y :: ty => (x, y) :: (combine tx ty)
+  end.
+
+Compute (combine [1;2] [false;false;true;true]).
+
+Inductive option (X:Type) : Type :=
+  | Some : X -> option X
+  | None : option X.
+
+Arguments Some {X} _.
+Arguments None {X}.
+Fixpoint index {X : Type} (n : nat)
+               (l : list X) : option X :=
+  match l with
+  | [] => None
+  | a :: l' => if beq_nat n O then Some a else index (pred n) l'
+  end.
+
+Example test_index1 : index 0 [4;5;6;7] = Some 4.
+Proof. reflexivity. Qed.
+Example test_index2 : index 1 [[1];[2]] = Some [2].
+Proof. reflexivity. Qed.
+Example test_index3 : index 2 [true] = None.
+Proof. reflexivity. Qed.
+
 Definition doit3times {X:Type} (f:X->X) (n:X) : X :=
   f (f (f n)).
+
+Fixpoint filter {X:Type} (test: X->bool) (l:list X)
+                : (list X) :=
+  match l with
+  | [] => []
+  | h :: t => if test h then h :: (filter test t)
+                        else filter test t
+  end.
+
+Example test_filter1: filter evenb [1;2;3;4] = [2;4].
+Proof. reflexivity. Qed.
+(* Definition length_is_1 {X : Type} (l : list X) : bool := *)
+(*   (length l) =? 1. *)
+(* Example test_filter2: *)
+(*     filter length_is_1 *)
+(*            [ [1; 2]; [3]; [4]; [5;6;7]; []; [8] ] *)
+(*   = [ [3]; [4]; [8] ]. *)
+(* Proof. reflexivity. Qed. *)
+
+Definition countoddmembers' (l:list nat) : nat :=
+  length (filter oddb l).
+Example test_countoddmembers'1: countoddmembers' [1;0;3;1;4;5] = 4.
+Proof. reflexivity. Qed.
+Example test_countoddmembers'2: countoddmembers' [0;2;4] = 0.
+Proof. reflexivity. Qed.
+Example test_countoddmembers'3: countoddmembers' nil = 0.
+Proof. reflexivity. Qed.
+
+(* Example test_filter2': *)
+(*     filter (fun l => (length l) =? 1) *)
+(*            [ [1; 2]; [3]; [4]; [5;6;7]; []; [8] ] *)
+(*   = [ [3]; [4]; [8] ]. *)
+(* Proof. reflexivity. Qed. *)
+
+Definition partition {X : Type}
+                     (test : X -> bool)
+                     (l : list X)
+                   : list X * list X
+(* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Fixpoint map {X Y: Type} (f:X->Y) (l:list X) : (list Y) :=
+  match l with
+  | [] => []
+  | h :: t => (f h) :: (map f t)
+  end.
+
+Theorem map_rev : forall(X Y : Type) (f : X -> Y) (l : list X),
+  map f (rev l) = rev (map f l).
+Proof.
+(* FILL IN HERE *) Admitted.
+
+Fixpoint fold {X Y: Type} (f: X->Y->Y) (l: list X) (b: Y)
+                         : Y :=
+  match l with
+  | nil => b
+  | h :: t => f h (fold f t b)
+  end.
+
+Definition constfun {X: Type} (x: X) : nat->X :=
+  fun (k:nat) => x.
+Definition ftrue := constfun true.
+
+Definition prod_curry {X Y Z : Type}
+  (f : X * Y -> Z) (x : X) (y : Y) : Z := f (x, y).
+
+Definition prod_uncurry {X Y Z : Type}
+  (f : X -> Y -> Z) (p : X * Y) : Z :=
+   admit.
+
+Theorem uncurry_curry : forall (X Y Z : Type) (f : X -> Y -> Z) x y,
+  prod_curry (prod_uncurry f) x y = f x y.
+Proof.
+Admitted.
+
+Theorem curry_uncurry : forall (X Y Z : Type)
+                               (f : (X * Y) -> Z) (p : X * Y),
+  prod_uncurry (prod_curry f) p = f p.
+Proof.
+Admitted.
+
+Definition option_map {X Y : Type} (f : X -> Y) (xo : option X)
+                      : option Y :=
+  match xo with
+    | None => None
+    | Some x => Some (f x)
+  end.
 
 Check plus.
 Definition plus3 := plus 3.
@@ -12,6 +262,9 @@ Proof. reflexivity. Qed.
 Example test_plus3'' : doit3times (plus 3) 0 = 9.
 Proof. reflexivity. Qed.
 
+Definition override {X: Type} (f: nat->X) (k:nat) (x:X) : nat->X:=
+  fun (k':nat) => if beq_nat k k' then x else f k'.
+
 Theorem unfold_example : forall m n,
   3 + n = m ->
   plus3 n + 1 = m + 1.
@@ -20,6 +273,44 @@ Proof.
   unfold plus3.
   rewrite -> H.
   reflexivity. Qed.
+
+Theorem beq_nat_refl : forall n : nat,
+  true = beq_nat n n.
+Proof.
+Admitted.
+
+Theorem override_eq : forall {X:Type} x k (f:nat->X),
+  (override f k x) k = x.
+Proof.
+  intros X x k f.
+  unfold override.
+  rewrite <- beq_nat_refl.
+  reflexivity. Qed.
+
+Theorem override_neq : forall {X:Type} x1 x2 k1 k2 (f : nat->X),
+  f k1 = x1 ->
+  beq_nat k2 k1 = false ->
+  (override f k2 x2) k1 = x1.
+Proof.
+Admitted.
+
+Theorem eq_add_S : forall (n m : nat),
+     S n = S m ->
+     n = m.
+Proof.
+  intros n m eq. inversion eq. reflexivity. Qed.
+
+Theorem silly4 : forall (n m : nat),
+     [n] = [m] ->
+     n = m.
+Proof.
+  intros n o eq. inversion eq. reflexivity. Qed.
+
+Theorem silly5 : forall (n m o : nat),
+     [n;m] = [o;o] ->
+     [n] = [m].
+Proof.
+  intros n m o eq. inversion eq. reflexivity. Qed.
 
 Theorem S_inj : forall (n m : nat) (b : bool),
      beq_nat (S n) (S m) = b ->
