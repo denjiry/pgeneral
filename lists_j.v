fc802af7bbde7e64549a93801ad248e3fd60baf0
kento tajiri <k.denjiry@gmail.com>
2019-03-01 04:15:23 +0900

    Update lists_j.v

--- lists_j.v
+++ lists_j.v
@@ -1,14 +1,214 @@
-Require Export basicsj.
+Require Export Basics_J.
+
+Module NatList.
 
 Inductive natlist : Type :=
   | nil : natlist
   | cons : nat -> natlist -> natlist.
+
+Inductive natprod : Type :=
+  pair : nat -> nat -> natprod.
+
+Definition fst (p : natprod) : nat :=
+  match p with
+  | pair x y => x
+  end.
+Definition snd (p : natprod) : nat :=
+  match p with
+  | pair x y => y
+  end.
+
+Notation "( x , y )" := (pair x y).
+
+Definition fst' (p : natprod) : nat :=
+  match p with
+  | (x,y) => x
+  end.
+Definition snd' (p : natprod) : nat :=
+  match p with
+  | (x,y) => y
+  end.
+
+Definition swap_pair (p : natprod) : natprod :=
+  match p with
+  | (x,y) => (y,x)
+  end.
+
+Theorem surjective_pairing' : forall (n m : nat),
+  (n,m) = (fst (n,m), snd (n,m)).
+Proof.
+  reflexivity. Qed.
+
+Theorem surjective_pairing : forall (p : natprod),
+  p = (fst p, snd p).
+Proof.
+  intros p. destruct p as (n,m). simpl. reflexivity. Qed.
+
+Theorem snd_fst_is_swap : forall (p : natprod),
+  (snd p, fst p) = swap_pair p.
+Proof.
+Admitted.
+
+(* Inductive natlist : Type := *)
+(*   | nil : natlist *)
+(*   | cons : nat -> natlist -> natlist. *)
 
 Definition l_123 := cons 1 (cons 2 (cons 3 nil)).
 
 Notation "x :: l" := (cons x l) (at level 60, right associativity).
 Notation "[ ]" := nil.
 Notation "[ x , .. , y ]" := (cons x .. (cons y nil) ..).
+
+Notation "x + y" := (plus x y)
+                    (at level 50, left associativity). 
+
+Fixpoint repeat (n count : nat) : natlist :=
+  match count with
+  | O => nil
+  | S count' => n :: (repeat n count')
+  end.
+
+Fixpoint length (l:natlist) : nat :=
+  match l with
+  | nil => O
+  | h :: t => S (length t)
+  end.
+
+Fixpoint app (l1 l2 : natlist) : natlist :=
+  match l1 with
+  | nil => l2
+  | h :: t => h :: (app t l2)
+  end.
+
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+
+Example test_app1: [1,2,3] ++ [4,5] = [1,2,3,4,5].
+Proof. reflexivity. Qed.
+Example test_app2: nil ++ [4,5] = [4,5].
+Proof. reflexivity. Qed.
+Example test_app3: [1,2,3] ++ nil = [1,2,3].
+Proof. reflexivity. Qed.
+
+Definition hd (default:nat) (l:natlist) : nat :=
+  match l with
+  | nil => default
+  | h :: t => h
+  end.
+
+Definition tail (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => t
+  end.
+
+Example test_hd1: hd 0 [1,2,3] = 1.
+Proof. reflexivity. Qed.
+Example test_hd2: hd 0 [] = 0.
+Proof. reflexivity. Qed.
+Example test_tail: tail [1,2,3] = [2,3].
+Proof. reflexivity. Qed.
+
+Definition bag := natlist.
+
+Theorem nil_app : forall l:natlist,
+  [] ++ l = l.
+Proof.
+   reflexivity. Qed.
+
+Theorem tl_length_pred : forall l:natlist,
+  pred (length l) = length (tail l).
+Proof.
+  intros l. destruct l as [| n l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = cons n l'".
+    reflexivity. Qed.
+
+Theorem app_ass : forall l1 l2 l3 : natlist,
+  (l1 ++ l2) ++ l3 = l1 ++ (l2 ++ l3).
+Proof.
+  intros l1 l2 l3. induction l1 as [| n l1'].
+  Case "l1 = nil".
+    reflexivity.
+  Case "l1 = cons n l1'".
+    simpl. rewrite -> IHl1'. reflexivity. Qed.
+
+Theorem app_length : forall l1 l2 : natlist,
+  length (l1 ++ l2) = (length l1) + (length l2).
+Proof.
+  intros l1 l2. induction l1 as [| n l1'].
+  Case "l1 = nil".
+    reflexivity.
+  Case "l1 = cons".
+    simpl. rewrite -> IHl1'. reflexivity. Qed.
+
+Fixpoint snoc (l:natlist) (v:nat) : natlist :=
+  match l with
+  | nil => [v]
+  | h :: t => h :: (snoc t v)
+  end.
+
+Fixpoint rev (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => snoc (rev t) h
+  end.
+
+Theorem length_snoc : forall n : nat, forall l : natlist,
+  length (snoc l n) = S (length l).
+Proof.
+  intros n l. induction l as [| n' l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = cons n' l'".
+    simpl. rewrite -> IHl'. reflexivity. Qed.
+
+Theorem rev_length : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = cons".
+    simpl. rewrite -> length_snoc.
+    rewrite -> IHl'. reflexivity. Qed.
+
+Inductive natoption : Type :=
+  | Some : nat -> natoption
+  | None : natoption.
+
+Fixpoint index_bad (n:nat) (l:natlist) : nat :=
+  match l with
+  | nil => 42 
+  | a :: l' => match beq_nat n O with
+               | true => a
+               | false => index_bad (pred n) l'
+               end
+  end.
+
+Fixpoint index (n:nat) (l:natlist) : natoption :=
+  match l with
+  | nil => None
+  | a :: l' => match beq_nat n O with
+               | true => Some a
+               | false => index (pred n) l'
+               end
+  end.
+
+Fixpoint index' (n:nat) (l:natlist) : natoption :=
+  match l with
+  | nil => None
+  | a :: l' => if beq_nat n O then Some a else index (pred n) l'
+  end.
+
+Definition option_elim (o : natoption) (d : nat) : nat :=
+  match o with
+  | Some n' => n'
+  | None => d
+  end.
+
+
 
 Theorem silly1 : forall (n m o p : nat),
      n = m ->
