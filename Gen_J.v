f158c0b861142da8dc6b978fec91b248146e6af8
kento tajiri <k.denjiry@gmail.com>
2019-03-02 03:45:48 +0900

    Ad Gen_J.v

---
+++ Gen_J.v
@@ -0,0 +1,53 @@
+Require Export Poly_J.
+
+Theorem double_injective_FAILED : forall n m,
+     double n = double m ->
+     n = m.
+Proof.
+  intros n m. induction n as [| n'].
+  Case "n = O". simpl. intros eq. destruct m as [| m'].
+    SCase "m = O". reflexivity.
+    SCase "m = S m'". inversion eq.
+  Case "n = S n'". intros eq. destruct m as [| m'].
+    SCase "m = O". inversion eq.
+    SCase "m = S m'".
+      assert (n' = m') as H.
+      SSCase "Proof of assertion".
+Admitted.
+
+Theorem double_injective' : forall n m,
+     double n = double m ->
+     n = m.
+Proof.
+  intros n. induction n as [| n'].
+  Case "n = O". simpl. intros m eq. destruct m as [| m'].
+    SCase "m = O". reflexivity.
+    SCase "m = S m'". inversion eq.
+  Case "n = S n'".
+    intros m eq.
+    destruct m as [| m'].
+    SCase "m = O". inversion eq.     SCase "m = S m'".
+      assert (n' = m') as H.
+      SSCase "Proof of assertion". apply IHn'.
+        inversion eq. reflexivity.
+      rewrite -> H. reflexivity. Qed.
+
+
+Theorem double_injective_take2 : forall n m,
+     double n = double m ->
+     n = m.
+Proof.
+  intros n m.
+  generalize dependent n.
+  induction m as [| m'].
+  Case "m = O". simpl. intros n eq. destruct n as [| n'].
+    SCase "n = O". reflexivity.
+    SCase "n = S n'". inversion eq.
+  Case "m = S m'". intros n eq. destruct n as [| n'].
+    SCase "n = O". inversion eq.
+    SCase "n = S n'".
+      assert (n' = m') as H.
+      SSCase "Proof of assertion".
+        apply IHm'. inversion eq. reflexivity.
+      rewrite -> H. reflexivity. Qed.
+
